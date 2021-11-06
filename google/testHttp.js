be1e28034706cf67a1c85312d3cb6166858ace39
AlexFreik <freik.aa@phystech.edu>
2021-11-06 16:53:12 +0100

    cleanning

--- google/testHttp.js
+++
@@ -1,9 +0,0 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-const Http = new XMLHttpRequest();
-const url='https://www.googleapis.com/drive/v2/files?q="1p6avfXtLQBAImA9teTjIBW1GYTSsOpsL"+in+parents&key={apiKey}';
-Http.open("GET", url);
-Http.send();
-
-Http.onreadystatechange = (e) => {
-  console.log(Http.responseText)
-}
